c934c6405802340e1a481f05a52d3e99c6813413
Regina <reginalin714@gmail.com>
2019-09-09 15:40:42 -0400

    refactor stopwatch to use context

---
+++ backend/static/react/precache-manifest.4b40b609f681d076424f693697db9064.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a829792bc14389bd677a0c818ab5e87",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "cce6c4c9bc106479bfbe",
+    "url": "/static/react/css/main.8b32cf33.chunk.css"
+  },
+  {
+    "revision": "05d2a48e9198bf5684d7",
+    "url": "/static/react/js/2.783b1a6b.chunk.js"
+  },
+  {
+    "revision": "cce6c4c9bc106479bfbe",
+    "url": "/static/react/js/main.0d67556b.chunk.js"
+  },
+  {
+    "revision": "b41af828d69188e69846",
+    "url": "/static/react/js/runtime~main.24f70ceb.js"
+  }
+]);
