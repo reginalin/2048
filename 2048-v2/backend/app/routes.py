2b5daa67e3099df735c1eaf493001ab73ac2fcae
Regina <reginalin714@gmail.com>
2019-09-14 23:24:08 -0400

    add separate section

---
+++ 2048-v2/backend/app/routes.py
@@ -0,0 +1,49 @@
+import os
+import json
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "highscores.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.form: 
+        form_name = request.form.get("name") 
+        if form_name == None:
+            form_name = "someone"
+        userscore = Score(name=form_name, 
+                scoreValue=request.form.get("scoreValue"))
+        db.session.add(userscore)
+        db.session.commit()
+    scores = top_scores(10)
+    top_scores_json = query_as_json(scores)
+    print("NOW SCORES")
+    print(top_scores_json)
+    return render_template("index.html", token=top_scores_json)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    scoreValue = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return "<Name: {}>".format(self.name)
+
+def query_as_json(scores):
+    scores_array = [ {
+                        "id": score.id,
+                        "name": score.name, 
+                        "scoreValue": score.scoreValue
+                    } 
+            for score in scores ]
+    return scores_array
+
+# get top 10 scores
+def top_scores(n): 
+    return Score.query.order_by(Score.scoreValue.asc()).limit(n)
---
+++ backend/static/react/precache-manifest.2a84bb26293bf2218ddb9cbff8606693.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5436d7d673bcfc32d9792409c5c637",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "e4d958b3f1ed0fcc83aace470b5c57e1",
+    "url": "/static/react//media/parrot.e4d958b3.gif"
+  },
+  {
+    "revision": "54f08e76fa7639c7e0e94775d2e8b809",
+    "url": "/static/react//media/party-parrot.54f08e76.gif"
+  },
+  {
+    "revision": "8eb4b50c242e2a32be0f",
+    "url": "/static/react/css/main.d854b98f.chunk.css"
+  },
+  {
+    "revision": "05d2a48e9198bf5684d7",
+    "url": "/static/react/js/2.783b1a6b.chunk.js"
+  },
+  {
+    "revision": "8eb4b50c242e2a32be0f",
+    "url": "/static/react/js/main.6613e682.chunk.js"
+  },
+  {
+    "revision": "b41af828d69188e69846",
+    "url": "/static/react/js/runtime~main.24f70ceb.js"
+  }
+]);
