213368438868a3778e1605ce26bdd12b52b2731b
Regina <reginalin714@gmail.com>
2019-09-16 12:13:43 -0400

    add config and init

---
+++ 2048-v2/backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_FILE = "sqlite:///{}".format(os.path.join(PROJECT_DIR, "highscores.db"))
+
+SQLALCHEMY_DATABASE_URI = DATABASE_FILE
+SQLALCHEMY_TRACK_MODIFICATIONS = False
