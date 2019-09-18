3c1d6937dff537c2e45c97328cc0f76355439a43
Regina <reginalin714@gmail.com>
2019-09-18 14:59:53 -0400

    rename theme context file for clarity

---
+++ frontend/src/themeContext.js
@@ -0,0 +1,73 @@
+/**
+ * Handle global application themes using React Context
+ */
+
+import React from 'react';
+import {THEMES} from './constants.js';
+
+const ThemeStateContext = React.createContext();
+const ThemeDispatchContext = React.createContext();
+
+/**
+ * Toggles theme state between (light, dark) and (normal, ultra)
+ */
+const themeReducer = (state, action) => {
+	switch (action) {
+		case THEMES.light:
+			document.documentElement.setAttribute("color-theme", 'dark');
+			return {color: THEMES.dark, fun: state.fun};
+		case THEMES.dark:
+			document.documentElement.setAttribute("color-theme", 'light');
+			return {color: THEMES.light, fun: state.fun};
+		case THEMES.ultra:
+			document.documentElement.setAttribute("fun-theme", 'normal');
+			return {color: state.color, fun: THEMES.normal};
+		case THEMES.normal:
+			document.documentElement.setAttribute("fun-theme", 'ultra');
+			return {color: state.color, fun: THEMES.ultra};
+		default: 
+			document.documentElement.setAttribute("color-theme", 'light');
+			document.documentElement.setAttribute("fun-theme", 'normal');
+			return {color: THEMES.light, fun: THEMES.normal};
+	}
+}
+
+/**
+ * Context provider for theme context 
+ * Gives theme context state and dispatch function to alter context state
+ */
+const ThemeProvider = ({children}) => {
+	const [state, dispatch] = 
+		React.useReducer(themeReducer, {color: THEMES.light, fun: THEMES.normal});
+	return (
+		<ThemeStateContext.Provider value={state}>
+			<ThemeDispatchContext.Provider value={dispatch}>
+				{children}
+			</ThemeDispatchContext.Provider>
+		</ThemeStateContext.Provider>
+	);
+}
+
+/**
+ * Custom hook providing theme context
+ */
+const useThemeState = () => {
+	const themeContext = React.useContext(ThemeStateContext);
+	if (themeContext === undefined) {
+		throw new Error('themeContext must be used within a ThemeProvider');
+	}
+	return themeContext;
+}
+
+/**
+ * Custom hook providing theme context
+ */
+const useThemeDispatch = () => {
+	const themeContext = React.useContext(ThemeDispatchContext);
+	if (themeContext === undefined) {
+		throw new Error('themeContext must be used within a ThemeProvider');
+	}
+	return themeContext;
+}
+
+export { ThemeProvider, useThemeState, useThemeDispatch };
